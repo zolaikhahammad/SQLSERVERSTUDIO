3b372deb22306f87046357eb6d1dc97f6dca9341
Zolaikha Hammad <70544823+zolaikhahammad@users.noreply.github.com>
2022-07-30 00:45:42 +0500

    Add files via upload

---
+++ BaseResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQLProject
+{
+    public class BaseResponse
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public string database { get; set; }
+        public string table { get; set; }
+
+        public string coloumn { get; set; }
+        public int selectedcol_index { get; set; }
+        public int fromcol_index { get; set; }
+        public int tocol_index { get; set; }
+        public string servername { get; set; }
+        public string authentication { get; set; }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SQLProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BusinessLogic.BusinessLogic logic = new BusinessLogic.BusinessLogic();
+        StandardWindow standard = new StandardWindow();
+        public MainWindow()
+        {
+            InitializeComponent();
+            comboBox.SelectedIndex = 0;
+        }
+
+        private void login_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+
+                string username = this.txtlogin.Text;
+                string password = this.txtpassword.Password;
+                string servername = this.txtserver.Text;
+                ComboBoxItem ComboItem = (ComboBoxItem)comboBox.SelectedItem;
+                string name = ComboItem.Name;
+                Models.Database database = new Models.Database();
+                database.databaselist = new List<string>();
+                database.database = "";
+                database.username = username;
+                database.password = password;
+                database.authentication = name;
+                database.servername = servername;
+                if (name == "SQL" && (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password)))
+                {
+                    MessageBox.Show("Please enter username and password", "Error", MessageBoxButton.OK);
+                }
+                else
+                {
+                    database.databaselist = logic.GetDatabaseList(servername, username, password, name);
+                    this.Close();
+                    standard.Initialize(database);
+
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButton.OK);
+            }
+        }
+        private bool handle = true;
+        private void ComboBox_DropDownClosed(object sender, EventArgs e)
+        {
+            if (handle) Handle();
+            handle = true;
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox cmb = sender as ComboBox;
+            handle = !cmb.IsDropDownOpen;
+            Handle();
+        }
+
+        private void Handle()
+        {
+            ComboBoxItem ComboItem = (ComboBoxItem)comboBox.SelectedItem;
+            string name = ComboItem.Name;
+            if (name == "WA")
+            {
+                txtlogin.IsEnabled = false;
+                txtpassword.IsEnabled = false;
+            }
+            else if (name == "SQL")
+            {
+                txtlogin.IsEnabled = true;
+                txtpassword.IsEnabled = true;
+            }
+
+        }
+    }
+}
